a6b2f0b3fe8f7ce59ee5bfa3d4eb7d72ea6c9d8e
arnauz94 <kil94@live.fr>
2020-07-10 12:00:21 +0200

    video stage

--- public/javascripts/cube.js
+++ public/javascripts/cube.js
@@ -2,6 +2,7 @@ var cube = document.querySelector('.cube');
 var currentClass = '';
 var oldElem = null;
 const doNotClick = 'do-not-click';
+const sourceToAdd = '<div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;"> <iframe style="width:100%;height:100%;position:absolute;left:0px;top:0px;overflow:hidden" frameborder="0" type="text/html" src="https://www.dailymotion.com/embed/video/x770zyb?autoplay=0" width="100%" height="100%" allowfullscreen> </iframe> </div>'
 
 function sleep(duration) {
 	return new Promise(resolve => {
@@ -26,18 +27,21 @@ function rotate(elem) {
     oldElem = elem;
 }
 
-async function init() {    
-    rotate(document.getElementById("front"));
-    await sleep(0.7);
+function displayVideo() {
+    var toReplace = document.getElementById("rapport-de-stage");
+    var videoElement = document.createElement('div');
+    videoElement.innerHTML = sourceToAdd.trim();
+    toReplace.replaceWith(videoElement);
+}
+
+async function init() {
     rotate(document.getElementById("back"));
-    await sleep(0.5);    
+    await sleep(0.5);     
     rotate(document.getElementById("bottom"));
-    await sleep(0.7);
-    rotate(document.getElementById("top"));
-    await sleep(0.5);
-    // rotate(document.getElementById("left"));
-    // await sleep(0.5);
+    await sleep(1);
     rotate(document.getElementById("front"));
+    await sleep(1);
+    displayVideo();
 
 }
 init();
