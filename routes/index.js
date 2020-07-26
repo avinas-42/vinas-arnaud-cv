6ed42ab6573d04e03c9163761d056fea05753de5
arnauz94 <kil94@live.fr>
2020-07-26 18:52:34 +0200

    /cv

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/cv');
+  var ua = req.header('user-agent');
+  // Check the user-agent string to identyfy the device. 
+  if(/mobile|iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(ua)) {
+    res.render('cv', { isMobile: true});
+  } else {
+    res.render('cv', { isMobile: false});
+  }
 });
 
 router.get('/cv', function(req, res, next) {
